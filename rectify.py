e942395f7d4dd603cb99053e2d247337d9350428
Alex Yang <aleozlx@gmail.com>
2017-11-29 16:58:15 -0600

    odd..

--- rectify.py
+++ rectify.py
@@ -40,6 +40,20 @@ def line_intersection(line1, line2):
     y = det(d, ydiff) / div
     return x, y
 
+def scale(A, s):
+    ret = A.copy()
+    ret[0,0] *= s
+    ret[1,1] *= s
+    return ret
+
+def translate(A, x, y):
+    ret = A.copy()
+    ret[0,2] += x
+    ret[1,2] += y
+    print('H =', ret)
+    return ret
+
+
 v1 = np.array(line_intersection([points[0], points[1]], [points[2], points[3]]) + (1,))
 v2 = np.array(line_intersection([points[0], points[2]], [points[1], points[3]]) + (1,))
 f = np.sqrt(1 - np.dot(v1, v2))
@@ -52,13 +66,15 @@ r3 = np.cross(r1, r2)
 print('<r1 r2>', np.dot(r1, r2))
 R = np.column_stack([r1, r2, r3])
 print('det(R) =', np.linalg.det(R))
-# H = np.dot(K, np.dot(np.linalg.inv(R), np.linalg.inv(K)))
-H = np.dot(K, np.dot(R, np.linalg.inv(K)))
-H[0,2] = H[1,2] = 100
-H[2,0] = H[2,1] = 0
-H /= H[2,2]
-print('H =', H)
-imOut = cv2.warpPerspective(im, H, (1024,768))
-print(imOut.shape)
-cv2.imshow('output', imOut)
+H = np.dot(K, np.dot(np.linalg.inv(R), np.linalg.inv(K)))
+# H = np.dot(K, np.dot(R, np.linalg.inv(K)))
+# H[2,0] = H[2,1] = 0
+print('(H) =', H)
+def showRectified(H, idx):
+    imOut = cv2.warpPerspective(im, H, (1024,768))
+    cv2.imshow('output%d'%idx, imOut)
+
+# showRectified(translate(scale(H, 0.5), -1200, 100), 0)
+showRectified(translate(scale(H, 0.5), -1200, -100), 1)
+# showRectified(translate(scale(H, 0.5), -1200, -200), 2)
 cv2.waitKey(0)
